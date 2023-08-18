098a48d541af1c36b18aa1ffeb4405ad0e539378
love20011994 <love.kalsangrah@gmail.com>
2023-08-18 20:03:51 +0530

    Register alert message add

--- src/components/Register.js
+++ src/components/Register.js
@@ -27,9 +27,17 @@ function Register() {
     
        console.log(formData);
       const Registerresponse=  await axios.post("http://localhost:5000/api/products/register/postapi",{formData})
+      if(Registerresponse.data.status=== "success"){
+        alert("registration sucess")
+      }
+      else{
+        console.log("hello")
+      }
    console.log(Registerresponse.data.token,"Responserr")
-
+  console.log(Registerresponse.data.status,"@####@@@@@@@@@@@##########@@@@@@@@")
    localStorage.setItem('token',Registerresponse.data.token)
+
+  
    
         // Reset form
         // setFormData({
@@ -43,7 +51,9 @@ function Register() {
         //   confirmPassword: ''
         // });
 
-
+        // if(Registerresponse.data.status===success){
+        //   alert("registration sucess")
+        // }
 
       };
 

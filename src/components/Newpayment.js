7a5e0165b5a4e62f24650b81ad8641e2abc4b5ca
love20011994 <love.kalsangrah@gmail.com>
2023-10-13 14:23:47 +0530

    firstcommit

--- src/components/Newpayment.js
+++ src/components/Newpayment.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+// import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 function Newpayment() {
   return (
-    <div style={{backgroundColor:"red",marginTop:"200px"}}>hello bhai</div>
+    <div style={{}}>
+
+<Carousel>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+    </div>
   )
 }
 
--- src/components/PaymentGateway.js
+++ src/components/PaymentGateway.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import axios  from 'axios';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function PaymentGateway() {
+  const [productdata,setproductdata] = useState([])
+
+ const data = async()=>{
+   const data1 = await axios.get("https://fakestoreapi.com/products")
+   console.log("love")
+
+   const data2 = await data1.data
+   console.log(data2,"@@@@@@@@@@@@@")
+   setproductdata(data2,)
+ }
+ console.log(productdata.title,"hweello")
+  useEffect(()=>{
+    data()
+  },[])
   return (
-    <div style={{backgroundColor:"red",marginTop:"200px"}}>PaymentGateway hai bhai</div>
+    <div style={{backgroundColor:"",marginTop:"8rem",marginRight:"2rem"}}>
+      <Carousel 
+      showStatus={false}
+      showIndicators={false}
+      showThumbs={false}
+      autoPlay
+      infiniteLoop
+      >
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2023/09/15/00/17/pink-8253910_1280.jpg" />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631_1280.jpg" />
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg" />
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+            </Carousel>
+            <Box sx={{display:"flex",justifyContent:"center",alignItems:"center",marginTop:"50px",marginLeft:"100px"}}>
+      <Grid container columnSpacing={4} rowSpacing={4} >
+        
+          
+          {productdata.map((p)=>{
+            return( <Grid  xs={12} sm={6} md={4} lg={4} xl={4}  >
+            <div style={{ width:"275px",height:"275px",display:"flex",justifyContent:"center", alignItems:"center",backgroundColor:"",marginBottom:"50px",padding:"50px"}}>
+            <img width={150} height={150} src={p.image}/>
+            </div>
+        </Grid>
+            )
+          })}
+        
+
+       
+        </Grid>
+    </Box>
+    </div>
   )
 }
 
